16b9bb79d82c821f212d4a030a15269c0bdf8ace
Efraim López <46459026+EfraimLA@users.noreply.github.com>
2022-05-26 18:16:34 -0500

    spring boot init

---
+++ src/main/java/org/acme/CursosAlternativosSbApplication.java
@@ -0,0 +1,13 @@
+package org.acme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursosAlternativosSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursosAlternativosSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/acme/CursosAlternativosSbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.acme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursosAlternativosSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
